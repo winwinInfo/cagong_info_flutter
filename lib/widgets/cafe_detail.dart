61d060e8f0c2956f6241a4619eecfee0c0f577f7
성현우 <yannoo@naver.com>
2024-08-08 15:26:43 +0900

    유튜브 및 카페 디테일 포맷 수정

--- lib/widgets/cafe_detail.dart
+++ lib/widgets/cafe_detail.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import '../models/cafe.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:youtube_player_iframe/youtube_player_iframe.dart'; // 수정됨
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class CafeDetail extends StatefulWidget {
   final Cafe cafe;
@@ -15,6 +14,7 @@ class CafeDetail extends StatefulWidget {
 
 class _CafeDetailState extends State<CafeDetail> {
   YoutubePlayerController? _controller;
+  bool _hasValidVideoUrl = false;
 
   @override
   void initState() {
@@ -30,15 +30,24 @@ class _CafeDetailState extends State<CafeDetail> {
         _controller = YoutubePlayerController.fromVideoId(
           videoId: videoId,
           autoPlay: false,
-          params: const YoutubePlayerParams(showFullscreenButton: true),
+          params: const YoutubePlayerParams(
+            showFullscreenButton: true,
+            mute: false,
+            showControls: true,
+            loop: false,
+          ),
         );
+        _hasValidVideoUrl = true;
       }
     }
   }
 
   @override
   void dispose() {
-    _controller?.close();
+    // 컨트롤러가 존재하고 초기화되었을 때만 close() 호출
+    if (_controller != null && _hasValidVideoUrl) {
+      _controller!.close();
+    }
     super.dispose();
   }
 
@@ -50,75 +59,114 @@ class _CafeDetailState extends State<CafeDetail> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(widget.cafe.name, // widget. 추가
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            // Text(widget.cafe.name,
+            //     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            _buildTextWithLineBreaks('${widget.cafe.message}'),
+
             SizedBox(height: 8),
-            Text(widget.cafe.name, // widget. 추가
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            _buildAddressAndNavigationButton(),
             SizedBox(height: 8),
-            Text('주소: ${widget.cafe.address}'), // widget. 추가
-            ElevatedButton(
-              child: Text('길찾기'),
-              onPressed: () =>
-                  _launchMaps(widget.cafe.id, widget.cafe.name), // widget. 추가
-            ),
-            Text('메시지: ${widget.cafe.message}'), // widget. 추가
-            Text('영업 시간: ${widget.cafe.openingHours}'), // widget. 추가
-            Text('평일 이용 시간: ${widget.cafe.hoursWeekday}시간'), // widget. 추가
-            Text('주말 이용 시간: ${widget.cafe.hoursWeekend}시간'), // widget. 추가
-            Text('가격: ${widget.cafe.price}원'), // widget. 추가
+
+            _buildTextWithLineBreaks('영업 시간: ${widget.cafe.openingHours}'),
+            _buildTextWithLineBreaks('평일 이용 시간: ${widget.cafe.hoursWeekday}시간'),
+            _buildTextWithLineBreaks('주말 이용 시간: ${widget.cafe.hoursWeekend}시간'),
+            _buildTextWithLineBreaks('가격: ${widget.cafe.price}원'),
             SizedBox(height: 16),
-            Text('좌석 정보:', style: TextStyle(fontWeight: FontWeight.bold)),
-            Table(
-              border: TableBorder.all(),
-              children: [
-                TableRow(children: [
-                  Text('종류', textAlign: TextAlign.center),
-                  Text('좌석 수', textAlign: TextAlign.center),
-                  Text('콘센트', textAlign: TextAlign.center),
-                ]),
-                ...widget.cafe.seatingInfo // widget. 추가
-                    .map((seat) => TableRow(children: [
-                          Text(seat.type, textAlign: TextAlign.center),
-                          Text(seat.count.toString(),
-                              textAlign: TextAlign.center),
-                          Text(seat.powerCount.toString(),
-                              textAlign: TextAlign.center),
-                        ]))
-                    .toList(),
-              ],
-            ),
+            _buildSeatingInfo(),
             SizedBox(height: 16),
-            if (_controller != null) // widget. 추가
-              YoutubePlayerControllerProvider(
-                controller: _controller!,
-                child: YoutubePlayer(
-                  controller: _controller!,
-                  aspectRatio: 16 / 9,
-                ),
-              ),
+            _buildVideoSection(),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildCouponInfo(String cafeName) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+  Widget _buildTextWithLineBreaks(String text) {
+    return Text(
+      text.replaceAll('\\n', '\n'),
+      style: TextStyle(height: 1.5), // 줄 간격을 조절합니다.
+    );
+  }
+
+  Widget _buildAddressAndNavigationButton() {
+    return Row(
       children: [
-        SizedBox(height: 16),
-        Text('쿠폰 정보',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        SizedBox(height: 8),
-        Image.network(
-          '${cafeName}쿠폰.png', // 쿠폰 이미지 URL을 적절히 수정해야 합니다.
-          errorBuilder: (context, error, stackTrace) {
-            return Text('쿠폰 이미지를 불러올 수 없습니다.');
-          },
+        Expanded(
+          child: Text(
+            '주소: ${widget.cafe.address}',
+            style: TextStyle(fontSize: 14),
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+          ),
+        ),
+        SizedBox(width: 16), // 텍스트와 버튼 사이의 간격
+        ElevatedButton(
+          child: Text('길찾기'),
+          onPressed: () => _launchMaps(widget.cafe.id, widget.cafe.name),
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
         ),
       ],
     );
+  }
+
+  Widget _buildSeatingInfo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('좌석 정보:', style: TextStyle(fontWeight: FontWeight.bold)),
+        Table(
+          border: TableBorder.all(),
+          children: [
+            TableRow(children: [
+              Text('종류', textAlign: TextAlign.center),
+              Text('좌석 수', textAlign: TextAlign.center),
+              Text('콘센트', textAlign: TextAlign.center),
+            ]),
+            ...widget.cafe.seatingInfo
+                .map((seat) => TableRow(children: [
+                      Text(seat.type, textAlign: TextAlign.center),
+                      Text(seat.count.toString(), textAlign: TextAlign.center),
+                      Text(seat.powerCount.toString(),
+                          textAlign: TextAlign.center),
+                    ]))
+                .toList(),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildVideoSection() {
+    if (_hasValidVideoUrl && _controller != null) {
+      return AspectRatio(
+        aspectRatio: 9 / 16,
+        child: YoutubePlayerControllerProvider(
+          controller: _controller!,
+          child: YoutubePlayer(
+            controller: _controller!,
+          ),
+        ),
+      );
+    } else {
+      return Card(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Icon(Icons.video_library, size: 48, color: Colors.grey),
+              SizedBox(height: 8),
+              Text(
+                '이 카페에 대한 동영상이 아직 없습니다.',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 
   void _launchMaps(String cafeId, String cafeName) async {
